4753f454e2be2b8c3be62a3c4dcd972c143f8d65
Mina Atef <mina2000atef@gmail.com>
2023-06-15 18:20:38 +0300

    Currency Fixed

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const Fetch = async () => {
     const data = await fetch('https://api.escuelajs.co/api/v1/products');
+    const Change = await fetch('http://data.fixer.io/api/latest?access_key=02cab87f61a2396af45180a0010e1424&format=1&_gl=1*18n83no*_ga*MTA5NzM0Mjc0Ny4xNjg2ODQxMzI1*_ga_HGV43FGGVM*MTY4Njg0MTMyNS4xLjEuMTY4Njg0MTU2My42MC4wLjA.')
+
+    const changeCur = await Change.json();
     const parsed = await data.json();
     const CategoriesMap = new Map();
+    console.log(changeCur.rates.EGP);
   
     parsed.forEach(obj => {
       const categoryId = obj.category.id;
       const product = {
         id: obj.id,
         title: obj.title,
-        price: obj.price,
+        price: `${(obj.price * changeCur.rates.EGP).toFixed(2)} EGP`,
         description: obj.description,
         category: {
           id: obj.category.id,
@@ -38,7 +42,7 @@ const Fetch = async () => {
 
 const Categories = Array.from(CategoriesMap.values());
 
-    console.log(Categories);
+    console.log(Categories[0]);
   };
   
   Fetch();
